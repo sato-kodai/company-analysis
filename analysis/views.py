e7e0904fc57141bb7da424c1d83cfb3690d611c4
sato-kodai <satoverphone@gmail.com>
2020-10-23 20:06:35 +0900

    決算推移表示機能の実装（ビューの調整はまだ）

--- analysis/views.py
+++ analysis/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import TemplateView
-from .models import Statement
+from django.views.generic import TemplateView, DetailView
+from .models import Company, Statement
 
-
-class Top(generic.TemplateView):
+class TopView(generic.TemplateView):
     template_name = 'top.html'
 
     def get_context_data(self, **kwargs):
-      statement_list = Statement.objects.all().order_by('company')
-      params = {'statement_list': statement_list,}
-      return params
+        company_list = Company.objects.all().order_by('id')
+        params = {'company_list': company_list,}
+        return params
+
+class CompanyView(DetailView):
+    model = Company
+    
+    def get_context_data(self, **kwargs):
+        company_name = kwargs['object'].name
+        statement_list = Statement.objects.filter(company=kwargs['object']).order_by('-fiscal_year')[:4]
+        params = {
+            'company_name': company_name,
+            'statement_list': statement_list,
+        }
+        return params
