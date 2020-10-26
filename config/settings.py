f5ac3b4ca6ebdcc0d30478aa34f0ebe9d53d9aec
sato-kodai <satoverphone@gmail.com>
2020-10-26 23:16:54 +0900

    STATIC_URL のコメントアウトを外す

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = '/user/share/nginx/html/static'
 MEDIA_ROOT ='/user/share/nginx/html/media'
