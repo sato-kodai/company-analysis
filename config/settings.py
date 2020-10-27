c0a507be9e9908371ad6017cd99fa966e8f8b6d9
sato-kodai <satoverphone@gmail.com>
2020-10-27 09:04:48 +0900

    settingsの誤字修正

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/user/share/nginx/html/static'
-MEDIA_ROOT ='/user/share/nginx/html/media'
+STATIC_ROOT = '/usr/share/nginx/html/static'
+MEDIA_ROOT ='/usr/share/nginx/html/media'
 
 AUTH_USER_MODEL = 'accounts.User'
 
