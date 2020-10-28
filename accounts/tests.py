d8822c495b7adbfbbaf2e17a2a9331ccbc107b24
sato-kodai <satoverphone@gmail.com>
2020-10-28 16:59:17 +0900

    ログイン機能のseleniumによるテスト実装

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-# Create your tests here.
+
+class LoginTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(executable_path='/Users/satokodai/portfolio/company-analysis/chromedriver')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get('http://localhost:8000' + str(reverse_lazy('login')))
+
+        # ログイン
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('username')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('password')
+        self.selenium.find_element_by_class_name('btn').click()
+
+        # ページタイトルの検証
+        self.assertEquals('Company Analysis', self.selenium.title)
