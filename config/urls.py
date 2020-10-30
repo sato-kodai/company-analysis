dd9e138295c0a845b965a3dd11558bbf9d715bbe
sato-kodai <satoverphone@gmail.com>
2020-10-30 13:12:34 +0900

    トップページの実装とそれに伴う各ページのCSS変更

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import static
+from . import settings_local
 
 urlpatterns = [
     path('', include('analysis.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+urlpatterns += static(settings_local.STATIC_URL, document_root=settings_local.STATIC_ROOT)
+urlpatterns += static(settings_local.MEDIA_URL, document_root=settings_local.MEDIA_ROOT)
