e0e3034b0d7aec4d25c647ac774cc5381d605f51
sato-kodai <satoverphone@gmail.com>
2020-10-30 13:48:31 +0900

    開発環境のDBをSOLiteからPostgresqlに変更

--- config/settings_local.py
+++ config/settings_local.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'company_analysis',
+        'USER': 'app_admin',
+        'PASSWORD': 'tech1234',
+        'HOST': '',
+        'PORT': 5432,
     }
 }
 
