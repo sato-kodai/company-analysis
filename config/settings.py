f63df500563a9b850e248a3d57b6f286564a7654
sato-kodai <satoverphone@gmail.com>
2020-10-31 14:50:38 +0900

    データベースの設定をコメントアウト

--- config/settings.py
+++ config/settings.py
@@ -185,7 +185,7 @@ EMAIL_BACKEND = 'django_ses.SESBackend'
 
 # heroku用
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
